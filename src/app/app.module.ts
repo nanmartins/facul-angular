659466ec85e25a3942857970375c3fd60d954701
nanmartins <renanmartins989@gmail.com>
2023-10-26 18:39:44 +0100

    first module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/Header/header.components';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    ComponentsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/components/components.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { HeaderComponent } from "./Header/header.components";
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  exports: [
+    HeaderComponent
+  ]
+})
+
+export class ComponentsModule {
+
+}
