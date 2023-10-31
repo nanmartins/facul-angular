284e9f0aeb02d5280f3bf1f47c94ed76a6374100
Renan Martins <91997504+nanmartins@users.noreply.github.com>
2023-10-31 18:27:33 +0000

    rxjs subscription

--- aulas/start/src/app/pages/rxjs/rxjs-page.component.ts
+++ aulas/start/src/app/pages/rxjs/rxjs-page.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from "@angular/core";
-import { interval, of } from "rxjs";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription, interval, of, } from "rxjs";
 
 @Component({
   selector: 'app-rxjs-page',
   templateUrl: './rxjs-page.component.html',
 })
 
-export class RXJSPageComponent implements OnInit {
+export class RXJSPageComponent implements OnInit, OnDestroy {
   items: Array<number> =[]
+
+  msg = ''
+  subscription!: Subscription
 
   // observable = of('item 1', 'item 2')
   observable = interval(1000)
@@ -15,8 +18,18 @@ export class RXJSPageComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    this.observable.subscribe((item) => {
+    this.subscription = this.observable.subscribe((item) => {
       this.items.push(item)
     })
   }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
+    this.msg = 'Unsubscribed'
+  }
+
+  // desinscrever() {
+  //   this.subscription.unsubscribe()
+  //   this.msg = 'Unsubscribed'
+  // }
 }
