da054b40768f628423de26aaf5edb39227d3e888
Renan Martins <91997504+nanmartins@users.noreply.github.com>
2023-10-31 18:41:41 +0000

    rxjs operators, and gitignore update

--- aulas/start/src/app/pages/rxjs/rxjs-page.component.ts
+++ aulas/start/src/app/pages/rxjs/rxjs-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
-import { Subscription, interval, of, } from "rxjs";
+import { Subscription, interval, of, map, tap, filter, range } from "rxjs";
 
 @Component({
   selector: 'app-rxjs-page',
@@ -14,13 +14,24 @@ export class RXJSPageComponent implements OnInit, OnDestroy {
 
   // observable = of('item 1', 'item 2')
   observable = interval(1000)
+  observable2 = range(1, 10)
 
   constructor() {}
 
   ngOnInit() {
-    this.subscription = this.observable.subscribe((item) => {
+    this.subscription = this.observable
+    .pipe(
+      map(x => x *2),
+      filter(x => x <= 10),
+      tap((x) => console.log(x))
+    )
+    .subscribe((item) => {
       this.items.push(item)
     })
+
+    // this.observable2.subscribe((item) => {
+    //   this.items.push(item)
+    // })
   }
 
   ngOnDestroy() {
